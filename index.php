906f3f610a8f8a13b64852221c88d2258ad6bf05
Taufik Nurrohman <hi@taufik-nurrohman.com>
2024-03-24 16:48:12 +0700

    Update

--- index.php
+++ index.php
@@ -228,75 +228,221 @@ asdf asdf asdf
       ~~~
    3. asdf asdf asdf
 5. asdf asdf asdf
+
+asdf asdf asdf
+
+- - -
+
+asdf asdf asdf
+
+- asdf asdf asdf
+  asdf asdf asdf
+
+  <asdf:asdf>
+
+  asdf asdf asdf
+
+      asdf asdf asdf
+      asdf asdf asdf
+
+      <asdf:asdf>
+
+      asdf asdf asdf
+
+  asdf asdf asdf
+
+  ```
+  asdf asdf asdf
+  asdf asdf asdf
+
+  <asdf:asdf>
+
+  asdf asdf asdf
+  ```
+
+  ``` asdf
+  asdf asdf asdf
+  asdf asdf asdf
+
+  <asdf:asdf>
+
+  asdf asdf asdf
+  ```
+
+  asdf asdf asdf
+
+  ~~~
+  asdf asdf asdf
+  asdf asdf asdf
+
+  <asdf:asdf>
+
+  asdf asdf asdf
+  ~~~
+
+  ~~~ asdf
+  asdf asdf asdf
+  asdf asdf asdf
+
+  <asdf:asdf>
+
+  asdf asdf asdf
+  ~~~
+- asdf asdf asdf
+- <asdf:asdf>
+- asdf asdf asdf
+  - asdf asdf asdf
+  - asdf asdf asdf
+    asdf asdf asdf
+
+    <asdf:asdf>
+
+    asdf asdf asdf
+
+        asdf asdf asdf
+        asdf asdf asdf
+
+        <asdf:asdf>
+
+        asdf asdf asdf
+
+    asdf asdf asdf
+
+    ```
+    asdf asdf asdf
+    asdf asdf asdf
+
+    <asdf:asdf>
+
+    asdf asdf asdf
+    ```
+
+    ``` asdf
+    asdf asdf asdf
+    asdf asdf asdf
+
+    <asdf:asdf>
+
+    asdf asdf asdf
+    ```
+
+    asdf asdf asdf
+
+    ~~~
+    asdf asdf asdf
+    asdf asdf asdf
+
+    <asdf:asdf>
+
+    asdf asdf asdf
+    ~~~
+
+    ~~~ asdf
+    asdf asdf asdf
+    asdf asdf asdf
+
+    <asdf:asdf>
+
+    asdf asdf asdf
+    ~~~
+  - asdf asdf asdf
+- asdf asdf asdf
+
+asdf asdf asdf
 MD;
 
-function chunk(string $content, int $d = 0) {
-    $prev = "";
-    $r = [];
-    foreach (\explode("\n", $content) as $row) {
+function blocks(string $content, int $d = 0) {
+    $block = -1;
+    $blocks = [];
+    $rows = \explode("\n", $content);
+    foreach ($rows as $row) {
         while (false !== ($before = \strstr($row, "\t", true))) {
             $v = \strlen($before);
             $row = $before . \str_repeat(' ', 4 - $v % 4) . \substr($row, $v + 1);
         }
         $prefix = \str_repeat(' ', $d);
-        if ($prefix === \substr($prev, 0, $d)) {
-            $prev = \substr($prev, $d);
-        }
         if ($prefix === \substr($row, 0, $d)) {
             $row = \substr($row, $d);
         }
         $dent = \strspn($row, ' ');
         if ("" === $row) {
-            $prefix = ""; // Do not indent empty chunk(s)
+            $prefix = ""; // Do not indent empty block
         }
-        if ("" !== $prev && isset($r[$last = \count($r) - 1])) {
-            if (("" === $row || $dent >= 4) && \strspn($r[$last][0], ' ') >= 4) {
-                $r[$last][0] .= "\n" . $row;
+        if ($prev = $blocks[$block][0] ?? 0) {
+            if (false !== \strpos('`~', $prev[0]) && ($n = \strspn($prev, $prev[0])) >= 3) {
+                if ($row === \str_repeat($prev[0], $n)) {
+                    $blocks[$block++][0] .= "\n" . $row;
+                    continue;
+                }
+                $blocks[$block][0] .= "\n" . $row;
+                $blocks[$block][1] = false;
                 continue;
             }
-            if ("" === $row || $dent > 0) {
-                $n = \strspn($prev, '0123456789');
-                if ($n > 9) {
-                    $r[] = [$prev = $prefix . $row, true];
-                    continue;
-                }
-                if (false !== \strpos(').', \substr($prev, $n, 1)) && false !== \strpos(" \t", \substr($prev, $n + 1, 1))) {
-                    $r[$last][0] .= "\n" . $prefix . $row;
-                    continue;
-                }
-                $r[] = [$prev = $prefix . $row, true];
-                continue;
-            }
-            if (isset($r[$last][0][0])) {
-                if ('>' === $r[$last][0][0] && '>' === \substr($row, 0, 1)) {
-                    $r[$last][0] .= "\n" . $prefix . $row;
-                    continue;
-                }
-                if (false !== \strpos('`~', $r[$last][0][0]) && ($n = \strspn($r[$last][0], $r[$last][0][0])) >= 3) {
-                    if ("\n" . \str_repeat($r[$last][0][0], $n) === \substr($r[$last][0], -($n + 1))) {
-                        $r[] = [$prev = $prefix . $row, true];
-                        continue;
+            if (false !== \strpos('*+-', $prev[0]) && false !== \strpos(" \t", $prev[1] ?? "")) {
+                if ("" !== $row && $dent < 2) {
+                    if ("\n" === \substr($prev, -1)) {
+                        // Convert last `\n` character in list block syntax to the next block if next row is not empty
+                        // and is not a list block syntax
+                        $blocks[$block][0] = \substr($blocks[$block][0], 0, -1);
+                        $blocks[++$block] = ["", false];
                     }
-                    $r[$last][0] .= "\n" . $prefix . $row;
-                    $r[$last][1] = false;
+                    $blocks[++$block] = [$row, "" !== $row];
                     continue;
                 }
+                $blocks[$block][0] .= "\n" . $row;
+                continue;
             }
-        }
-        if ($dent >= 4) {
-            $r[] = [$prev = $prefix . $row, false];
+            $n = \strspn($prev, '0123456789');
+            if ($n > 9) {
+                $blocks[++$block] = [$row, "" !== $row];
+                continue;
+            }
+            if (false !== \strpos(').', \substr($prev, $n, 1)) && false !== \strpos(" \t", \substr($prev, $n + 1, 1))) {
+                if ("" !== $row && $dent < 3) {
+                    if ("\n" === \substr($prev, -1)) {
+                        // Convert last `\n` character in list block syntax to the next block if next row is not empty
+                        // and is not a list block syntax
+                        $blocks[$block][0] = \substr($blocks[$block][0], 0, -1);
+                        $blocks[++$block] = ["", false];
+                    }
+                    $blocks[++$block] = [$row, "" !== $row];
+                    continue;
+                }
+                $blocks[$block][0] .= "\n" . $row;
+                continue;
+            }
+            if (\strspn($prev, ' ') >= 4) {
+                if ("" !== $row && $dent < 4) {
+                    if ("\n" === \substr($prev, -1)) {
+                        // Convert last `\n` character in indented code block syntax to the next block if next row is
+                        // not empty and is not an indented code block syntax
+                        $blocks[$block][0] = \substr($blocks[$block][0], 0, -1);
+                        $blocks[++$block] = ["", false];
+                    }
+                    $blocks[++$block] = [$row, "" !== $row];
+                    continue;
+                }
+                $blocks[$block][0] .= "\n" . $row;
+                $blocks[$block][1] = false;
+                continue;
+            }
+            if ("" === $row) {
+                $blocks[++$block] = ["", false];
+                continue;
+            }
+            $blocks[$block][0] .= "\n" . $row;
             continue;
         }
-        $r[] = [$prev = $prefix . $row, true];
+        $blocks[++$block] = [$row, "" !== $row];
     }
-    return $r;
+    return $blocks;
 }
 
 // $content = '    ' . \strtr($content, ["\n" => "\n    "]);
 // $content = \preg_replace('/^[ \t]+$/m', "", $content);
 
 echo '<pre>';
-foreach (chunk($content) as $v) {
+foreach (blocks($content) as $v) {
     echo '<span style="border:2px solid;color:#' . ($v[1] ? '080' : '800') . ';display:block;margin:0 0 1px;">' . (\htmlspecialchars($v[0]) ?: '<br>') . '</span>';
 }
 echo '</pre>';
